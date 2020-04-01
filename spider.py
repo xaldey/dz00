cd485f1cfb6acacdcafae8de06b437713e3b2d76
xaldey <fguzkp25@gmail.com>
2020-04-01 19:18:00 +0300

    Осталось только добавить выгрузку в пакет whl

--- spider.py
+++ spider.py
@@ -17,10 +17,6 @@ HEADERS = {'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHT
 date_stamp = datetime.datetime.today().strftime("%Y-%m-%d-%H-%M-%S-")
 
 
-search_links = []
-recursive_search_array = []
-
-
 # Сначала ищем в Яндекс
 serp_string = input('Что ищем?: ')
 serp_results = input('Сколько результатов найти?: ')
@@ -31,52 +27,68 @@ result_type = input('Укажите конечный формат для рез�
 result_type = 'csv'
 result_file_serp = str(date_stamp + '--result.' + result_type)
 result_file_recursive = str(date_stamp + '--result-recursive.' + result_type)
+search_links = []
+recursive_search_array = []
 # print(result_file_serp)
-if rec_search:
-    print(result_file_recursive)
-else:
-    print(result_file_serp)
 
 
 def main():
-    response = requests.get(search_url, headers=HEADERS)
-    response.encoding = 'utf-8'
-    print(search_url)
-    if response.ok:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        li = soup.find_all('li', class_='serp-item')
-        for el in li:
-            link = el.find('a').get('href')
-            name = el.find('a').get_text()
-            result_link = [name, link]
-            search_links.append(result_link)
-        print(len(search_links))
-        # print(search_links)
-        csv.writer(open(result_file_serp, "w", newline='')).writerows(search_links)
+    if rec_search:
+        print(result_file_recursive)
+        print('Рекурсивный поиск запущен!')
+        response = requests.get(search_url, headers=HEADERS)
+        # response.encoding = 'cp1251'
+        print(search_url)
+        if response.ok:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            li = soup.find_all('li', class_='serp-item')
+            for el in li:
+                link = el.find('a').get('href')
+                name = el.find('a').get_text()
+                result_link = [name, link]
+                search_links.append(result_link)
+                # csv.writer(open(result_file_serp, "w", newline='')).writerows(search_links)
+            print(len(search_links))
+        else:
+            print('Error!')
     else:
-        print('Error!')
+        print(result_file_serp)
+        print('Поисковая выдача запущена!')
 
 
 def recursive_search():
     look_for = search_links[0][1]
     print('Парсим сайт: ' + look_for)
     recursive_response = requests.get(look_for, headers=HEADERS)
-    recursive_soup = BeautifulSoup(recursive_response.text, 'html.parser')
-    for el in recursive_soup.findAll('a'):
-        link = str(el.get('href'))
-        name = str(el.get_text())
-        # name = el.find('a').get_text()
-        result_link = [link, name]
-        recursive_search_array.append(result_link)
-        print(result_link)
-        csv.writer(open(result_file_recursive, "w", newline='')).writerows(recursive_search_array)
-    print(len(recursive_search_array))
+    if recursive_response.ok:
+        recursive_soup = BeautifulSoup(recursive_response.text, 'html.parser')
+        # a_list = recursive_soup("a")
+        # print(a_list)
+        for el in recursive_soup.findAll('a'):
+            link = str(el.get('href'))
+            name = str(el.get_text())
+            result_link = [name, link]
+            recursive_search_array.append(result_link)
+            #print(result_link)
+            csv.writer(open(result_file_recursive, "w", newline='')).writerows(recursive_search_array)
+        print(len(recursive_search_array))
+    else:
+        print('error! cant get response')
+
+
+def result_export():
+    if result_type == 'csv':
+        print('CSV export!')
+        for el in search_links:
+            csv.writer(open(result_file_serp, "w", newline='')).writerows(search_links)
+    elif result_type == 'json':
+        pass
+    else:
+        print('Console output!')
+        print(search_links)
 
 
 if __name__ == '__main__':
     main()
     recursive_search()
-    if rec_search:
-        print('Рекурсивный поиск завершен!')
-    else:
-        print('Поисковая выдача завершена!')
+    result_export()
